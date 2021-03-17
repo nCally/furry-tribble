9bda0b18e0faecc6da4bff918895b1af87ea2dfb
Cally Nnamani <cartrf@live.com>
2021-03-17 12:05:05 +0100

    basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,53 @@
 import React from 'react';
-import logo from './logo.svg';
+import Leaflet from 'leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import beacons from './beacons.json';
+import icon from './beacon-icon';
 import './App.css';
+import beaconIcon from './beacon-icon';
+
+
+
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <section className="App">
+      <header>
+        <p>2 Survey Jobs</p>
       </header>
-    </div>
+      <main>
+        <MapContainer
+          className="map"
+          zoom={11}
+          scrollWheelZoom={true}
+          attributionControl={true}
+          center={[6.460188, 7.584494]}>
+          <TileLayer
+            id="mapbox/streets-v11"
+            url={`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibmNhbGx5IiwiYSI6ImNrN3Vud3N5bzE1cWwzbXFyc3lpaHlldWoifQ.-HWevPOSU8PIR6HSP4R8Lw`} />
+
+          {
+            beacons.map(beacon => (
+              <Marker
+                icon={beaconIcon}
+                position={
+                  [Number(beacon.latitude), Number(beacon.longitude)]
+                }></Marker>
+            )
+            )
+          }
+        </MapContainer>
+      </main>
+      <footer>
+        <ul>
+          <li>
+            <a href="#">Map View</a>
+          </li>
+        </ul>
+      </footer>
+    </section>
   );
 }
 
---
+++ src/beacon-icon.ts
@@ -0,0 +1,8 @@
+import L from 'leaflet';
+
+const beaconIcon = L.divIcon({
+  className: "k2",
+  html: '<div class="custom-icon"><div></div></div>'
+})
+
+export default beaconIcon;
